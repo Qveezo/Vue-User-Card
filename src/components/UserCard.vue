30114f4af54b569666bdd93d007936212152ec1f
Qveezo <keynpro228@gmail.com>
2022-12-07 20:08:37 +0300

    Добавлены основные файлы

---
+++ src/components/UserCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="informer-container">
+</div>
+</template>
+
+<script>
+export default{
+  name: 'Block',
+  data(){
+    return {text: 'Я новый компонент!'}
+  }
+}
+</script>
+
+<style>
+
+</style>
